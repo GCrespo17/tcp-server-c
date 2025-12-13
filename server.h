2c3a226a225669b42c40d6db67396da9039cd44a
gcrespo <gcrespo1705@gmail.com>
2025-12-12 20:37:10 -0400

    created header for the server with socket created and binded

---
+++ server.h
@@ -0,0 +1,11 @@
+#ifndef H_SERVER
+#define H_SERVER
+
+#include <sys/socket.h>
+int createSocket(int domain, int type, int protocol);
+
+int bindSocket(int socketFileDecriptor, const struct sockaddr *addr);
+
+
+
+#endif
